6fdac0d598137fc25ae4f12e20f08b4202989672
Shubham Sourabh <39551118+vampirepapi@users.noreply.github.com>
2021-06-26 01:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C://Users//vampirepapi//Desktop//nowhere//Internship//flask//todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
